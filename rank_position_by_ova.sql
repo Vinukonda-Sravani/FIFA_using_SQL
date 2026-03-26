e0ef9ea13b866a1a69f2b770a0a620a12250cf39
Vinukonda Sravani <vinukondasravani2002@gmail.com>
2026-03-26 22:02:33 +0530

    Delete rank_position_by_ova.sql

--- rank_position_by_ova.sql
+++
@@ -1,7 +0,0 @@
---rank players within each position by ova.showing top 3 per position
-select * from (
-select "Name","primary_position","↓OVA",
-       dense_rank() over(partition by "primary_position" order by "↓OVA") as ranky
-from players
-)
-where ranky<=3;
