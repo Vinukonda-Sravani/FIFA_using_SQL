799b8546f2f32058b7648925432f911e63048a7c
Vinukonda Sravani <vinukondasravani2002@gmail.com>
2026-03-26 22:02:19 +0530

    Delete position_by_avgova.sql

--- position_by_avgova.sql
+++
@@ -1,7 +0,0 @@
---average OVA by primary position.
--- only positions with more than 200 players.
-select "primary_position", round(AVG("↓OVA"),2) as avg_ova
-from players
-group by "primary_position"
-having count(*) > 200
-order by avg_ova desc;
