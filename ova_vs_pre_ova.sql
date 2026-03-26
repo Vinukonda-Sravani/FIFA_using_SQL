664ae6f424c3dfa94a8e8aaae987b60ccce7ebd6
Vinukonda Sravani <vinukondasravani2002@gmail.com>
2026-03-26 22:01:45 +0530

    Delete ova_vs_pre_ova.sql

--- ova_vs_pre_ova.sql
+++
@@ -1,5 +0,0 @@
--- for each club showing players order by ova with the prev players ova and the diff
-select "Club",player,"↓OVA",prev_players,("↓OVA"-prev_players) as diff
-from (select "Club","Name" as player,"↓OVA",
-lag("↓OVA") over(partition by "Club" order by "↓OVA" desc) as prev_players
-from players)p1;
