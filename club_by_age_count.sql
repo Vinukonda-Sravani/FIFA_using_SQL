86b4146fd7e19dab4ffaa4f09aa09bf657c7b5b7
Vinukonda Sravani <vinukondasravani2002@gmail.com>
2026-03-26 22:01:02 +0530

    Delete club_by_age_count.sql

--- club_by_age_count.sql
+++
@@ -1,7 +0,0 @@
--- How many players per nationality.
---showing top 10 by player count.
-select "Nationality",count(*) as player_count
-from players
-group by "Nationality"
-order by player_count desc
-limit 10;
