2f3bd75103c69aae2ffb5378298e597ceba04dc1
Vinukonda Sravani <vinukondasravani2002@gmail.com>
2026-03-26 22:01:24 +0530

    Delete club_total_bill.sql

--- club_total_bill.sql
+++
@@ -1,6 +0,0 @@
---whihc club has the highest total wages bill
---showing top clubs.
-select "Club",SUM(COALESCE("wage_eur",0)) as total_wage_bill
-from players
-group by "Club"
-Order by total_wage_bill desc;
